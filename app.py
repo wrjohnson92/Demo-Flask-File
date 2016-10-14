3202c6c81e4187200e9edc16d81c9e09c8b49eef
wrjohnson92 <wrjohnson92@gmail.com>
2016-10-13 22:16:29 -0500

    test

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Hello World"
+    return "Hello World"
+
+
+@app.route("/testing")
+def testing():
+    return "Testing"
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
